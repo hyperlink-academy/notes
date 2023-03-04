f75f2aeb0ae6656c1bde08504bd80fe5359f188c
Jared Pereira <jared@awarm.space>
2023-03-04 18:44:25 -0500

    add note header

--- app/note/[note]/page.tsx
+++ app/note/[note]/page.tsx
@@ -4,8 +4,20 @@ const Home = async (props: { params: { note: string } }) => {
   //I need to do this to make sure notes are cached in this function!
   await fs.readdir(path.join(process.cwd(), "./notes"));
   let Content = await import(`notes/${props.params.note}`);
-  console.log(Content);
-  return <Content.default />;
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="w-full ">
+        <h1 className="text-2xl font-bold">{Content.metadata.title}</h1>
+        <p className="italic text-grey-55">by {Content.metadata.author}</p>
+        <div>
+          {Content.metadata.tags.map((tag: string) => (
+            <span className="text-xs p-1 border rounded-md">{tag}</span>
+          ))}
+        </div>
+      </div>
+      <Content.default />
+    </div>
+  );
 };
 
 export default Home;
