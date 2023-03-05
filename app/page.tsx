f6061a4fb2a675f7a7948e9cad2ccad7c276d5d7
Jared Pereira <jared@awarm.space>
2023-03-05 02:04:51 -0500

    just some small tweaks

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
 export const metadata = { title: "notes.hyperlink.academy" };
 const Home = async () => {
-  return <div>Some notes from the hyperlink team</div>;
+  return (
+    <div>
+      Some notes from the hyperlink team. You can grab an rss feed{" "}
+      <a href="/rss">here</a>
+    </div>
+  );
 };
 
 export default Home;
