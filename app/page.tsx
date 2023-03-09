674aed1eab2d20415a63ae31e86ae7105c974a9e
Jared Pereira <jared@awarm.space>
2023-03-09 01:01:14 -0500

    move homepage to mdx

--- app/page.tsx
+++ app/page.tsx
@@ -1,53 +1,10 @@
 export const metadata = { title: "notes.hyperlink.academy" };
+
+import HomeContent from "./home.mdx";
 const Home = async () => {
   return (
     <div className="flex flex-col gap-4">
-      <p>Hi, welcome!</p>
-      <p>
-        We made this site to collect weeknotes, research, and ephemera — things
-        we generate as we build Hyperlink and explore learning futures.
-      </p>
-      <p>
-        Our first year, we made a little{" "}
-        <a href="https://year-one.hyperlink.academy/library">library</a>. The
-        next year, we wove a wiki in{" "}
-        <a href="https://space.hyperlink.academy/299766632440398340">
-          hyperspace
-        </a>
-        . This year, a lab notebook.
-      </p>
-      <p>
-        Among other things, we're trying{" "}
-        <a href="https://interconnected.org/home/2018/07/24/weeknotes">
-          weeknotes
-        </a>
-        , writing the kind of thing we enjoy reading. We write primarily for
-        ourselves, but hope you find something interesting here too.
-      </p>
-      <p>
-        Things we're thinking about: attention, intention, boundaries, play,
-        visuals and vibes, rooms and READMEs, co-presence and conversation,
-        presence and aliveness, sessions, sprints, cycles, cadences…
-      </p>
-      <p>
-        Last year was exploratory: research, evolution, new foundations. Now,
-        we're finding focus in what we're making, playing with new ways of
-        working, having fun with process.
-      </p>
-      <p>~-~-~-~</p>
-      <div>
-        {/* <p>~-~-~-~</p> */}
-        <p>Brendan, Jared, Celine</p>
-        <p>March, 2023</p>
-      </div>
-      <p>~-~-~-~</p>
-
-      <p>
-        <em>
-          PS — if you'd like an RSS feed of these notes, you can grab that{" "}
-          <a href="/rss">here</a>.
-        </em>
-      </p>
+      <HomeContent />
     </div>
   );
 };
