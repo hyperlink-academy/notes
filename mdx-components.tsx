608cb272171206eb331dbbd4daa8af473ad97ab3
Brendan Schlagel <brendan.schlagel@gmail.com>
2023-03-20 21:46:16 -0400

    added a new component YouTubeEmbed and added embeds for W10 and W11 videos

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,3 +1,15 @@
+function YouTubeEmbed(props: { id: string }) {
+  return (
+    <iframe
+      style={{ aspectRatio: "16/9", width: "100%" }}
+      src={`https://www.youtube.com/embed/${props.id}`}
+      title="YouTube video player"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowFullScreen
+    ></iframe>
+  );
+}
+
 export function useMDXComponents(components: { [k: string]: React.Component }) {
-  return { ...components };
+  return { YouTubeEmbed: YouTubeEmbed, ...components };
 }
