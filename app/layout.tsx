9b6a20e67dfb8b83b5b48beab18cdc864cc1d883
Brendan Schlagel <brendan.schlagel@gmail.com>
2023-06-14 19:19:32 -0400

    layout tweaks, bigger cards, more metadata in card list

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,14 +8,20 @@ export default async function RootLayout({
 }) {
   return (
     <html lang="en" className="w-full">
-      <body className="">
-        <div className="p-2 flex flex-col gap-2">
-          <div className="flex flex-row justify-between">
-            <Link href="/">home</Link>
-            <Link href="/about">about</Link>
-          </div>
-          {children}
+      {/* page wrapper */}
+      <body className="px-4 pb-4 max-w-3xl m-auto flex flex-col gap-4">
+        {/* <div className="px-4 pb-4 flex flex-col gap-2 max-w-3xl m-auto"> */}
+        {/* header nav bar */}
+        {/* shadow matches card shadow */}
+        <div
+          style={{ boxShadow: "3px 4px 0px 0px rgb(0 0 0 / 0.5)" }}
+          className="flex flex-row justify-between border-b border-grey-80 shadow border-l border-r rounded-b-md px-4 pt-4 pb-2 -mt-2 bg-white"
+        >
+          <Link href="/">home</Link>
+          <Link href="/about">about</Link>
         </div>
+        {children}
+        {/* </div> */}
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -3,38 +3,49 @@ import fs from "fs/promises";
 import Link from "next/link";
 import { cache } from "react";
 
-export default async function () {
-  return (
-    <div className="flex flex-col gap-4">
-      <NotesList />
-    </div>
-  );
+export default async function HomePage() {
+  return <NotesList />;
 }
 
 async function NotesList() {
   let pages = await getPageData();
   return (
-    <div className="m-auto flex max-w-4xl gap-8 sm:gap-16 p-4 sm:p-8">
-      <div className="w-24 sm:w-32 flex flex-col flex-shrink-0 gap-8 leading-5 sticky self-start top-8">
-        {pages
-          .sort((a, b) =>
-            a.metadata.published < b.metadata.published ? 1 : -1
-          )
-          .map((page) => (
-            // rotate each card from random -10 to 10 degrees
-            <Link
-              href={`/note/${page.filename.slice(0, -4)}`}
-              key={page.filename}
-              style={{
-                rotate: `${Math.floor(Math.random() * (10 - -10) + -10)}deg`,
-                boxShadow: "3px 4px 0px 0px rgb(0 0 0 / 0.5)",
-              }}
-              className="text-grey-15 hover:scale-105 transition-all no-underline border border-grey-80 rounded-md p-4 min-h-[72px] shadow bg-white"
-            >
-              {page.metadata?.title || page.filename}
-            </Link>
-          ))}
-      </div>
+    <div className="m-auto flex flex-col gap-8 sm:gap-16 p-4">
+      {pages
+        .sort((a, b) => (a.metadata.published < b.metadata.published ? 1 : -1))
+        .map((page) => (
+          // rotate each card from random -10 to 10 degrees
+          // translate each card (x-axis) from random -100 to 100 px
+          <Link
+            href={`/note/${page.filename.slice(0, -4)}`}
+            passHref
+            key={page.filename}
+            style={{
+              rotate: `${Math.floor(Math.random() * (10 - -10) + -10)}deg`,
+              translate: `${Math.floor(Math.random() * (100 - -100) + -100)}px`,
+              boxShadow: "3px 4px 0px 0px rgb(0 0 0 / 0.5)",
+            }}
+            className="text-grey-15 hover:scale-105 hover:border-grey-35 transition-all no-underline border border-grey-80 rounded-md p-4 min-h-[72px] shadow bg-white w-48 sm:w-64"
+          >
+            {/* card metadata wrapper */}
+            <div className="flex flex-col gap-2">
+              <h2 className="text-lg">
+                {page.metadata?.title || page.filename}
+              </h2>
+              <p className="text-sm">{page.metadata?.description}</p>
+              <div>
+                {page.metadata.tags.map((tag: string, index: number) => (
+                  <span
+                    className="text-xs p-1 border border-grey-55 bg-accent-gold rounded-md"
+                    key={index}
+                  >
+                    {tag}
+                  </span>
+                ))}
+              </div>
+            </div>
+          </Link>
+        ))}
     </div>
   );
 }
