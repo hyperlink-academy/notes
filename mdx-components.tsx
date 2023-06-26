341a2c4b7bd21b1e764d7e4ff1927b1df1baa2d9
Brendan Schlagel <brendan.schlagel@gmail.com>
2023-06-26 18:30:57 -0400

    remove buttondown subscribe for now

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -94,38 +94,38 @@ export function VisualPlaceholder(props: any) {
   return <div className="bg-white p-4 border h-48">{props.children}</div>;
 }
 
-export function ButtondownSubscribeForm(props: any) {
-  return (
-    <div className="flex flex-col gap-4 bg-white rounded-md p-4 text-center my-8 max-w-fit m-auto">
-      <p className="font-bold">sign up for Hyperlink&apos;s newsletter!</p>
-      <form
-        action="https://buttondown.email/api/emails/embed-subscribe/hyperlink"
-        method="post"
-        target="popupwindow"
-        onSubmit={
-          typeof window !== "undefined" &&
-          window.open("https://buttondown.email/hyperlink", "popupwindow")
-        }
-        className="embeddable-buttondown-form flex gap-2 items-center m-auto"
-      >
-        <label htmlFor="bd-email" className="italic">
-          email:
-        </label>
-        <input
-          type="email"
-          name="email"
-          id="bd-email"
-          className="p-2 rounded-md border"
-        />
-        <input
-          type="submit"
-          value="subscribe"
-          className="bg-accent-blue p-2 rounded-md text-white hover:cursor-pointer hover:bg-grey-15"
-        />
-      </form>
-    </div>
-  );
-}
+// export function ButtondownSubscribeForm(props: any) {
+//   return (
+//     <div className="flex flex-col gap-4 bg-white rounded-md p-4 text-center my-8 max-w-fit m-auto">
+//       <p className="font-bold">sign up for Hyperlink&apos;s newsletter!</p>
+//       <form
+//         action="https://buttondown.email/api/emails/embed-subscribe/hyperlink"
+//         method="post"
+//         target="popupwindow"
+//         onSubmit={
+//           typeof window !== "undefined" &&
+//           window.open("https://buttondown.email/hyperlink", "popupwindow")
+//         }
+//         className="embeddable-buttondown-form flex gap-2 items-center m-auto"
+//       >
+//         <label htmlFor="bd-email" className="italic">
+//           email:
+//         </label>
+//         <input
+//           type="email"
+//           name="email"
+//           id="bd-email"
+//           className="p-2 rounded-md border"
+//         />
+//         <input
+//           type="submit"
+//           value="subscribe"
+//           className="bg-accent-blue p-2 rounded-md text-white hover:cursor-pointer hover:bg-grey-15"
+//         />
+//       </form>
+//     </div>
+//   );
+// }
 
 export function useMDXComponents(components: { [k: string]: React.Component }) {
   return {
@@ -134,7 +134,7 @@ export function useMDXComponents(components: { [k: string]: React.Component }) {
     Quote,
     CTA,
     VisualPlaceholder,
-    ButtondownSubscribeForm,
+    // ButtondownSubscribeForm,
     ...components,
   };
 }
