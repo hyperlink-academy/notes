b895d5c90481ff49b815bcb2ec578349f82a9f9b
Brendan Schlagel <brendan.schlagel@gmail.com>
2023-09-07 18:41:49 -0400

    many lil tweaks to styles and Hyperlink Together post

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -95,7 +95,7 @@ TBD
 */
 export function Sidebar(props: any) {
   return (
-    <div className="bg-background p-4 border-2 border-accent-gold rounded-md flex flex-col gap-4 my-4">
+    <div className="bg-white p-4 border-8 border-accent-gold rounded-md flex flex-col gap-4 my-4">
       {props.children}
     </div>
   );
@@ -117,38 +117,26 @@ export function Caption(props: any) {
   );
 }
 
-// export function ButtondownSubscribeForm(props: any) {
-//   return (
-//     <div className="flex flex-col gap-4 bg-white rounded-md p-4 text-center my-8 max-w-fit m-auto">
-//       <p className="font-bold">sign up for Hyperlink&apos;s newsletter!</p>
-//       <form
-//         action="https://buttondown.email/api/emails/embed-subscribe/hyperlink"
-//         method="post"
-//         target="popupwindow"
-//         onSubmit={
-//           typeof window !== "undefined" &&
-//           window.open("https://buttondown.email/hyperlink", "popupwindow")
-//         }
-//         className="embeddable-buttondown-form flex gap-2 items-center m-auto"
-//       >
-//         <label htmlFor="bd-email" className="italic">
-//           email:
-//         </label>
-//         <input
-//           type="email"
-//           name="email"
-//           id="bd-email"
-//           className="p-2 rounded-md border"
-//         />
-//         <input
-//           type="submit"
-//           value="subscribe"
-//           className="bg-accent-blue p-2 rounded-md text-white hover:cursor-pointer hover:bg-grey-15"
-//         />
-//       </form>
-//     </div>
-//   );
-// }
+export function Button(props: { content: string; href: string }) {
+  return (
+    <a
+      href={props.href}
+      target="_blank"
+      rel="noopener noreferrer"
+      className={`
+      flex w-max gap-2
+      items-center justify-center
+      border py-1 px-2 rounded-md
+      font-bold text-white no-underline
+      border-accent-blue bg-accent-blue 
+      hover:bg-bg-blue hover:text-accent-blue
+      active:bg-bg-blue active:text-accent-blue active:outline active:outline-2
+      `}
+    >
+      {props.content}
+    </a>
+  );
+}
 
 export function useMDXComponents(components: { [k: string]: React.Component }) {
   return {
@@ -160,7 +148,7 @@ export function useMDXComponents(components: { [k: string]: React.Component }) {
     VisualPlaceholder,
     Image,
     Caption,
-    // ButtondownSubscribeForm,
+    Button,
     ...components,
   };
 }
